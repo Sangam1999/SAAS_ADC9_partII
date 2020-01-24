f55e161b944dd6b6ff6b8e3d7bac5f93c92ac58b
amitmagar <58926334+amitmagar@users.noreply.github.com>
2020-01-24 15:21:14 +0545

    Model_Relationships

--- WebApp/models.py
+++ WebApp/models.py
@@ -21,11 +21,43 @@ class Book(models.Model):
     number_of_children=models.IntegerField()
     arrival=models.DateField()
     checkOut=models.DateField()
+
+
+
+
+class Guest(models.Model):
+    name=models.CharField(max_length=20)
+    email=models.CharField(null=True, max_length=50)
+    phone=models.IntegerField()
+
+    def __str__(self):
+        return f"the guest in {self.Hotel}"
+
+class Room(models.Model):
+    number=models.IntegerField()
+    level=models.IntegerField()
+    types=models.CharField(max_length=20)
+    guests = models.ForeignKey(Guest,on_delete = models.CASCADE)
+    booking = models.ManyToManyField(Book) # booking may of many rooms at the same time one room may have multiple bookings
+
+
+    def __str__(self):
+        return f"the  {self.Hotel}contains room"
+
+
+
+
+class Hotel(models.Model):
+    hotel_name = models.CharField(max_length = 50)
+    place = models.CharField(max_length = 50)
+    guests = models.ManyToManyField(Guest)
     payment = models.CharField( 
         max_length = 20, 
         choices = payment_choices, 
         default = 'IMEPay'
         ) 
     comment=models.TextField(null=True)
+    room = models.ForeignKey(Room,on_delete = models.CASCADE)
     def __str__(self):
         return self.name
+
