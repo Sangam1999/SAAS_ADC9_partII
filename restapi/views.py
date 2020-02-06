0ef5a4a405fe873ff02b8294cf143bffd959dd31
yakhha012 <57519719+yakhha012@users.noreply.github.com>
2020-02-06 11:09:49 +0545

    postAPImodified

--- restapi/views.py
+++ restapi/views.py
@@ -23,7 +23,7 @@ def view_get_post_book(request):
         python_dictionary_object = json.loads(request.body)
         print("Python body contents =>",request.python_dictionary_object)
         print("Python body type =>",type(request.python_dictionary_object))
-        Book.objects.create(name=python_dictionary_object['name'],email=python_dictionary_object['name'],phone=python_dictionary_object['phone'],number_of_adults=python_dictionary_object['number_of_adults'],number_of_children=python_dictionary_object['number_of_children'],arrival=python_dictionary_object['arrival'],checkOut=python_dictionary_object['checkOut'])
+        Book.objects.create(name=python_dictionary_object['name'],email=python_dictionary_object['email'],phone=python_dictionary_object['phone'],number_of_adults=python_dictionary_object['number_of_adults'],number_of_children=python_dictionary_object['number_of_children'],arrival=python_dictionary_object['arrival'],checkOut=python_dictionary_object['checkOut'])
         return JsonResponse({
             "message":"Successfully posted"
         })
