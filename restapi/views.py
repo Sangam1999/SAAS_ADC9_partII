f2093d28196768776f1501c1003a7b659182a7f6
yakhha012 <57519719+yakhha012@users.noreply.github.com>
2020-02-10 12:48:25 +0545

    DeleteAPimodified

--- restapi/views.py
+++ restapi/views.py
@@ -50,6 +50,17 @@ def view_getByID_updateByID_deleteByID(request,ID):
         return JsonResponse({
             "message":"Successfully deleted"
         })
+    elif request.method == 'PUT':
+        books=Book.objects.get(id=ID)
+        books.name=request.POST['name']
+        books.email=request.POST['email']
+        books.phone=request.POST['phone']
+        books.number_of_adults=request.POST['number_of_adults']
+        books.number_of_children=request.POST['number_of_children']
+        books.arrival=request.POST['arrival']
+        books.checkOut=request.POST['checkOut']
+        books.save()
+
     else:
         return JsonResponse({
             "message":"Other http verbs testings"
